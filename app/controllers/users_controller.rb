65c8abc212d7d6d61c7b13a69d89e2734a8d110b
Leandro-Barretoo <officialdevmi@gmail.com>
2021-02-23 14:52:10 +0000

    Add and fixed user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,26 @@
 class UsersController < ApplicationController
     def new
-  @user= User.new
+      @user= User.new
     end
 
     def create
-
+      @user = User.create(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to root_path
+      else
+        render :new
+      end
     end
 
     def show
 
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name)
     end
 
 end
