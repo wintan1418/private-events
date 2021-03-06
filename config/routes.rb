e076086e2d4a9f0ecfba9866f072278ae5ac2da8
Leandro Barreto <72808808+Leandro-Barretoo@users.noreply.github.com>
2021-03-06 13:09:15 +0000

    implement changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
-  resources :users
+  resources :users, only: [:new, :create, :show]
   resources :events do
     resources :attendences, only: [:create]
   end
