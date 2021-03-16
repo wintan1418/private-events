4ad77bf56b926e5000b08ef0569156f71e43ea1f
oluwadare <wintan1418@gmail.com>
2021-03-16 15:37:25 +0100

    removed logics from folder helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,2 @@
 module ApplicationHelper
-    def layout_logic
-        <%= render "shared/navb" %>
-        <% if notice %>
-          <%= notice %>
-        <% end %>
-        <% if alert %>
-          <%= alert %>
-        <% end %>
-        <%= yield %>
-    end
 end
