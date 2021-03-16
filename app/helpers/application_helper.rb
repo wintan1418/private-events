b8904c0cd3628542b0e3cc16f176ed3a6672b79b
oluwadare <wintan1418@gmail.com>
2021-03-16 20:08:49 +0100

    add functionalities to the nav.html.erb helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,20 @@
 module ApplicationHelper
 
+
+    def nav_links
+        links = ' '
+        if logged_in?
+            links << link_to(link_to "MyEvents", user_path(current_user.id), class: "nav-link")
+            links << link_to(link_to "Log Out", logout_path, method: :delete, class: "nav-link")
+
+        else
+            links << link_to(link_to "Login", login_path, class: "nav-link")
+            links << link_to(link_to "Sign Up", new_user_path, class: "nav-link")
+
+        end
+         links.html_safe
+    end
+
     def logic
         links = ''
         if logged_in?
@@ -7,13 +22,8 @@ module ApplicationHelper
             links << ' | '
             links << link_to( "Attend", event_attendences_path(event_id: ev.id), method: :post)
             links << link_to( "Create Event", new_event_path, class: "text-center")
-        else
-            links << ' | '
-            links << link_to("Login", login_path, class: "nav-link")
-            links << link_to("Sign Up", new_user_path, class: "nav-link")
+        end
         links.html_safe
-         end
-
         end
     end
 
