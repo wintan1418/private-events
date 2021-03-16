1707b79237b3b6abec0f09ece83fc7eada9fc82c
oluwadare <wintan1418@gmail.com>
2021-03-16 18:53:13 +0100

    add more logics to the helpers folder and make the view cleaner

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
-end
+
+    def logic
+        links = ''
+        if logged_in?
+            links << link_to("Attend", event_attendences_path(event_id: ev.id), method: :post )
+            links << ' | '
+            links << link_to( "Attend", event_attendences_path(event_id: ev.id), method: :post)
+            links << link_to( "Create Event", new_event_path, class: "text-center")
+        else
+            links << ' | '
+            links << link_to("Login", login_path, class: "nav-link")
+            links << link_to("Sign Up", new_user_path, class: "nav-link")
+        links.html_safe
+         end
+
+        end
+    end
+
