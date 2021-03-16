6fb1b6f1902d13d8e300237c1f7bb76452a57698
oluwadare <wintan1418@gmail.com>
2021-03-16 14:29:47 +0100

    move logic from view folder to helper folder

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def layout_logic
+        <%= render "shared/navb" %>
+        <% if notice %>
+          <%= notice %>
+        <% end %>
+        <% if alert %>
+          <%= alert %>
+        <% end %>
+        <%= yield %>
+    end
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,26 @@
 module EventsHelper
+
+ def event_index
+  <ul>
+        <% @upcoming_events.each do |ev| %>
+        <li><%= ev.description %><br><%= ev.date %></li><%= link_to "View", event_path(id: ev.id) %><br>
+        <% if logged_in? %>
+            <%= link_to "Attend", event_attendences_path(event_id: ev.id), method: :post %>
+        <% end %>
+        <% end %>
+    </ul>
+    <h2>Here is a list of past events:</h2>
+    <ul>
+            <% @past_events.each do |ev| %>
+            <li><%= ev.description %><br><%= ev.date %></li><%= link_to "View", event_path(id: ev.id) %><br>
+            <% if logged_in? %>
+                <%= link_to "Attend", event_attendences_path(event_id: ev.id), method: :post %>
+            <% end %>
+            <% end %>
+    </ul>
+    <% if logged_in? %>
+    <%= link_to "Create Event", new_event_path, class: "text-center" %> 
+
+end
+
 end
