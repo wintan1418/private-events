91ea371ca4c5e0b920d0186d74e386f624bb05e8
oluwadare <wintan1418@gmail.com>
2021-03-17 10:46:51 +0100

    add final changes to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,17 @@
 module ApplicationHelper
 
+    def alert_notice
+        links = ' '
+        if notice
+        links << link_to(link_to  notice)  
+        end  
+    end
+    def notice_alert
+        links = ' '
+        if alert
+        links << link_to(link_to  alert)  
+        end  
+    end
 
     def nav_links
         links = ' '
@@ -15,12 +27,13 @@ module ApplicationHelper
          links.html_safe
     end
 
+   
+
     def logic
         links = ''
         if logged_in?
             links << link_to("Attend", event_attendences_path(event_id: ev.id), method: :post )
             links << ' | '
-            links << link_to( "Attend", event_attendences_path(event_id: ev.id), method: :post)
             links << link_to( "Create Event", new_event_path, class: "text-center")
         end
         links.html_safe
