3cbf40cad246a9e19b0a1622497a2e7d94b01a09
oluwadare <wintan1418@gmail.com>
2021-03-17 16:23:59 +0100

    fix linter errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,41 +1,35 @@
 module ApplicationHelper
   def alert_notice
     links = ' '
-      if notice
-        links << link_to(link_to(notice))
-      end
+    links << link_to(link_to(notice)) if notice
   end
 
   def notice_alert
     links = ' '
-      if alert
-        links << link_to(link_to(alert))
-      end
+    links << link_to(link_to(alert)) if alert
   end
 
   def nav_links
     links = ' '
-      if logged_in?
-        links << link_to(link_to('MyEvents', user_path(current_user.id), class: 'nav-link'))
-          links << link_to(link_to('Log Out', logout_path, method: :delete, class: 'nav-link'))
+    if logged_in?
+      links << link_to(link_to('MyEvents', user_path(current_user.id), class: 'nav-link'))
+      links << link_to(link_to('Log Out', logout_path, method: :delete, class: 'nav-link'))
 
-      else
-        links << link_to(link_to('Login', login_path, class: 'nav-link'))
-          links << link_to(link_to('Sign Up', new_user_path, class: 'nav-link'))
+    else
+      links << link_to(link_to('Login', login_path, class: 'nav-link'))
+      links << link_to(link_to('Sign Up', new_user_path, class: 'nav-link'))
 
-      end
-      links.html_safe
+    end
+    links.html_safe
   end
 
   def logic
     links = ''
-      if logged_in?
-        links << link_to('Attend', event_attendences_path(:event_id), method: :post)
-          links << link_to('Create ', new_event_path, class: 'text-center')
-       else
-            links << link_to(link_to('Login', login_path, class: 'nav-link'))
-              links << link_to(link_to('Sign Up', new_user_path, class: 'nav-link'))
-      end
-      links.html_safe
-      end
+    if logged_in?
+      links << link_to('Attend', event_attendences_path(:event_id), method: :post)
+      links << link_to('Create Event ', new_event_path, class: 'text-center')
+
     end
+    links.html_safe
+  end
+end
